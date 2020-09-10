049d50e1a8c8fb9969a509493eafa3582b564639
Jonathan Robijn <jrobijn@qualogy.com>
2020-09-10 10:55:08 +0200

    Implemented task 2

--- src/main/scala/BatchJob.scala
+++ src/main/scala/BatchJob.scala
@@ -4,13 +4,13 @@ import org.apache.spark.sql.functions._
 object BatchJob {
   def main(args: Array[String]) {
     // Read the command line arguments to determine the input file and output dir
-    val inputFile = if (args.size > 0) args(0) else "../routes.dat"
+    val inputFile = if (args.size > 0) args(0) else "../in/routes.dat"
     val outputDir = if (args.size > 1) args(1) else "../out/"
 
     val spark = SparkSession.builder.appName("Departure Airports Batch Job").getOrCreate()
     
     // Read in the specified input file using the custom schema specified in CustomSchema
-    val flightsDf = spark.read
+    val departureDf = spark.read
         .option("sep", ",")
         .option("inferSchema", "false")
         .option("header", "false")
@@ -19,7 +19,7 @@ object BatchJob {
         .cache()
     
     // Group the data by source airport and count the number of entries per group
-    val departureCountsDf = flightsDf.groupBy("source_airport").count()
+    val departureCountsDf = departureDf.groupBy("source_airport").count()
     val mostPopularDeparturesDf = departureCountsDf.orderBy(desc("count")).limit(10)
     
     // Print the table with the 10 most popular departures for debugging purposes 
---
+++ src/main/scala/StreamingJob.scala
@@ -0,0 +1,31 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object StreamingJob {
+  def main(args: Array[String]) {
+    // Read the command line arguments to determine the input dir
+    val inputDir = if (args.size > 0) args(0) else "../in/"
+
+    val spark = SparkSession.builder.appName("Departure Airports Streaming Job").getOrCreate()
+    
+    // Read in the files in the specified directory as an inputstream, using the custom schema specified in CustomSchema
+    val departureStream = spark.readStream
+        .option("sep", ",")
+        .schema(CustomSchema.schema)
+        .csv(inputDir)
+    
+    // Group the data by source airport and count the number of entries per group
+    val departureCountsDf = departureStream.groupBy("source_airport").count()
+    val mostPopularDeparturesDf = departureCountsDf.orderBy(desc("count")).limit(10)
+    
+    // Print the table with the 10 most popular departures for debugging purposes
+    // Ideally, we'd write to a file, but without time based aggregations we can't do this
+    val query = mostPopularDeparturesDf.writeStream
+        .outputMode("complete")
+        .format("console")
+        .start()
+    
+    query.awaitTermination
+    spark.stop()
+  }
+}
